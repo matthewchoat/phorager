0f911d32637eef2b6bef7f618db59e5708c60049
matthewchoat <matthewchoat@gmail.com>
2019-08-25 18:06:11 +0000

    editing and updating grams

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,35 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :delete]
 
+  def update
+    @post = Post.find_by_id(params[:id])
+    return render_not_found if @post.blank?
+
+    @post.update_attributes(post_params)
+
+    if @post.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
     @post = Post.new
   end
 
   def index
   end
+
+  def show
+    @post = Post.find_by_id(params[:id])
+    return render_not_found if @post.blank?
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+    return render_not_found if @post.blank?
+    end
 
   def create
     @post = current_user.posts.create(post_params)
@@ -21,6 +44,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+
+  describe "posts#update action" do
+    it "should allow users to successfully update posts" do
+      post = FactoryBot.create(:post, message: "Initial Value")
+      patch :update, params: { id: post.id, post: { message: 'Changed' } }
+      expect(response).to redirect_to root_path
+      post.reload
+      expect(post.message).to eq "Changed"
+    end
+
+    it "should have http 404 error if the post cannot be found" do
+      patch :update, params: { id: "PHONEYTEST", post: { message: 'Changed' } }
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with an http status of unprocessable_entity" do
+      post = FactoryBot.create(:post, message: "Initial Value")
+      patch :update, params: { id: post.id, post: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      post.reload
+      expect(post.message).to eq "Initial Value"
+    end
+  end
+
+ describe "posts#edit action" do
+    it "should successfully show the edit form if the post is found" do
+      post = FactoryBot.create(:post)
+      get :edit, params: { id: post.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error message if the post is not found" do
+      get :edit, params: { id: 'RANDOMPOST' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+
+  describe "posts#show action" do
+    it "should successfully show the page if the post is found" do
+      post = FactoryBot.create(:post)
+      get :show, params: { id: post.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the post is not found" do
+      get :show, params: { id: 'RANDOMPOST' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "posts#index action" do
     it "should successfully show the page" do
       get :index
